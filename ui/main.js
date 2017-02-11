e8fb13edf220475bc329ad7e9404820c9308c077
debjde6400 <debanjandey64@gmail.com>
2017-02-11 23:16:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,16 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'A new text';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function (){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
+    
     
 };
