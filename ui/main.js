ca295a6934c3f98c4ce21e2079a5c5f67aca5f88
debjde6400 <debanjandey64@gmail.com>
2017-02-11 23:08:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the main text
 var element = document.getElementById('main-text');
 element.innerHTML = 'A new text';
+
+var img = document.getElementById('madi');
+img.onclick = function (){
+    img.style.marginLeft = '100px';
+    
+};
