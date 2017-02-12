c71b839bac81ae53082cbde02afecbadb3c4996d
debjde6400 <debanjandey64@gmail.com>
2017-02-12 23:53:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,6 +121,13 @@ app.get('/ui/09376_HD.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '09376_HD.jpg'));
 });
 
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    var name= res.params.name;
+    name.push(name);
+    //json convet js objs to st
+    res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
