ac991824dc390511bc0ab3d08778d6a61a3aba7d
debjde6400 <debanjandey64@gmail.com>
2017-02-13 00:13:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,16 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    //var name= req.params.name;
+    var name= req.query.name;
+    names.push(name);
+    //json convet js objs to st
+    res.send(JSON.stringify(names));
+});
+
+
 app.get('/:articleName', function (req,res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
