4217199911336db3c6fde37014062a431e1a5ec3
debjde6400 <debanjandey64@gmail.com>
2017-02-13 00:39:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,12 +42,27 @@ button.onclick = function(){
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function(){
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for(var i=0; i < names.length; i++){
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML= list;
+    //create a request object, capture the response and store in a variable, render the variable in correct span
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            //take some action
+            if(request.status == 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i < names.length; i++){
+                    list += '<li>'+names[i]+'</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML= list;
+            }
+        }
+        //not done yet
+    };
+    //make the request
+    request.open('GET','http://debjde6400.imad.hasura-app.io/submit-name?name='+ name,true);
+    request.send(null);
 };
