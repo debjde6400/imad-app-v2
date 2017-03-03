40ec3665851c49b7910304427b130d0039f73e60
debjde6400 <debanjandey64@gmail.com>
2017-03-03 22:53:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user : 'debjde6400',
@@ -50,6 +51,17 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    //to create a hash
+    var hashed = crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'a-chosen-text');
+    res.send(hashedString);
 });
 
 var pool = new Pool(config);
